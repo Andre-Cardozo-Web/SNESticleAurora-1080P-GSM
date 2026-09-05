0145528efe06457a096c0184f09d9ca011a11bb3
Andre-Cardozo-Web <andre.do.polistation@gmail.com>
2026-09-04 22:07:25 -0300

    Update uiVideo.cpp

--- src/platform/ps2/ui/uiVideo.cpp
+++ src/platform/ps2/ui/uiVideo.cpp
@@ -158,7 +158,6 @@ void CVideoScreen::Input(Uint32 buttons, Uint32 trigger)
                 AudMixPceSetVolume(v); 
             } 
             break;
-
         case 55: 
             SnesAudioSetRate(SNSPCDSP_SAMPLERATE); 
             if (_pSystem == _pSnes && _AudMix) _AudMix->SetSampleRate(SNSPCDSP_SAMPLERATE); 
