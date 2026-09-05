b124abdacc6399ac1cf71c762b4efc63eae13f13
Andre-Cardozo-Web <andre.do.polistation@gmail.com>
2026-09-04 21:06:36 -0300

    Update uiVideo.cpp

--- src/platform/ps2/ui/uiVideo.cpp
+++ src/platform/ps2/ui/uiVideo.cpp
@@ -9,7 +9,7 @@
 #include "mainloop_menu.h"
 #include "mainloop_ui.h"
 
-// Caminhos globais corrigidos direto a partir do barramento raiz do console
+// Caminhos corrigidos apontando direto para a raiz do barramento system
 #include "system/mainloop_exec.h"
 #include "system/picodrive_bridge.h"
 #include "system/quicknes_bridge.h"
